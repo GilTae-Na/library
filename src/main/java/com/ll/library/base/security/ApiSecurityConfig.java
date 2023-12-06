047200c4872fc4f725090fc70c7e986693df1f58
nagiltae <nagt1997@gmail.com>
2023-12-06 15:43:58 +0900

    fix : member get test

--- src/main/java/com/ll/library/base/security/ApiSecurityConfig.java
+++ src/main/java/com/ll/library/base/security/ApiSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ll.library.base.security;
 
 
+import com.ll.library.base.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -17,23 +19,31 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(
+                        authorizeHttpRequests -> authorizeHttpRequests
+                                .requestMatchers("/api/*/member/login").permitAll()
+                                //로그인은 누구나 가능
+                                .anyRequest().authenticated()
+                        //나머지는 인증된 사용자만
+                )
                 .cors().disable() // 타 도메인에서 API 호출 가능
                 .csrf().disable() // CSRF 토큰 끄기
                 .httpBasic().disable() // httpBaic 로그인 방식 끄기
                 .formLogin().disable() // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
-                ); // 세션끄기
+                ) // 세션끄기
+                .addFilterBefore(
+                        jwtAuthorizationFilter, // 엑세스 토큰으로 부터 로그인 처리
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
